a51e63128714f23ad4ea7e5205cece9771f2d4d4
dadashov-o4 <157504744+dadashov-o4@users.noreply.github.com>
2024-05-25 08:18:35 +0400

    added article part

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -8,9 +8,9 @@ const Article = ({meqaleninBasligi,meqaleninMetni,meqaleninSekli}) => {
    <img src={meqaleninSekli}className="card-img-top" alt="..." />
    <div className="card-body">
      <h5 className="card-title">{meqaleninBasligi} </h5>
-     <p className="card-text">{meqaleninMetni}</p>
+     <p className="card-text">{meqaleninMetni='Sigh, it’s like we can’t catch a break. So much has happened over the past happened happened over the past happened.....'}</p>
      <p>
-       <img src="/assets/images/favicon.svg" alt="" />
+       <img className='' src="/assets/images/favicon.svg" alt="" />
        <span className='ms-3'>Pecode Team</span>
        <span id='tarix'>16.05.2024</span>
      </p>
